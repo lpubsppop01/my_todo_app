abb2ba968576766f9221d0e7941537101c695a6a
lpubsppop01 <lpubsppop01@gmail.com>
2019-09-21 14:39:51 +0900

    Implement add and remove of task

--- my_todo_app/app/main_window.py
+++ my_todo_app/app/main_window.py
@@ -6,6 +6,8 @@
 import tkinter as tk
 import tkinter.messagebox as ttk_messagebox
 import tkinter.scrolledtext as tk_scrolledtext
+import uuid
+from datetime import datetime
 from tkinter import ttk
 from typing import *
 
@@ -39,6 +41,7 @@ class MainWindow:
         self._root.grid_columnconfigure(0, weight=0)
         self._root.grid_columnconfigure(1, weight=0)
         self._root.grid_columnconfigure(2, weight=1)
+        self._root.bind('<Any-KeyPress>', self._key_pressed)
 
         # style = ttk.Style(self._root)
         # style.theme_use('classic')
@@ -79,7 +82,8 @@ class MainWindow:
         center_top_frame.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.E),
                               padx=(margin_half, margin_half), pady=(margin, margin_half))
 
-        add_task_button = ttk.Button(center_top_frame, text='Add')
+        add_task_button = ttk.Button(center_top_frame, text='Add',
+                                     command=self._add_task_button_clicked)
         add_task_button.grid(row=0, column=0, sticky=tk.E)
 
         add_task_button = ttk.Button(center_top_frame, text='Up')
@@ -105,16 +109,18 @@ class MainWindow:
         move_task_button = ttk.Button(right_top_frame, text='Move')
         move_task_button.grid(row=0, column=0, sticky=tk.E)
 
-        remove_task_button = ttk.Button(right_top_frame, text='Remove')
+        remove_task_button = ttk.Button(right_top_frame, text='Remove',
+                                        command=self._remove_task_button_clicked)
         remove_task_button.grid(row=0, column=1, sticky=tk.E)
 
-        self._name_entry = ttk.Entry(right_frame, font=name_font)
-        self._name_entry.grid(row=1, column=0, sticky=(tk.N, tk.S, tk.E, tk.W),
-                              padx=(margin_half, margin), pady=(margin, margin_half))
+        self._task_name_entry = ttk.Entry(right_frame, font=name_font)
+        self._task_name_entry.grid(row=1, column=0, sticky=(tk.N, tk.S, tk.E, tk.W),
+                                   padx=(margin_half, margin), pady=(margin, margin_half))
+        self._task_name_entry.bind("<FocusOut>", self._task_name_entry_focused_out)
 
-        self._memo_text = tk_scrolledtext.ScrolledText(right_frame, font=memo_font)
-        self._memo_text.grid(row=2, column=0, sticky=(tk.N, tk.S, tk.E, tk.W),
-                             padx=(margin_half, margin), pady=(margin_half, margin))
+        self._task_memo_text = tk_scrolledtext.ScrolledText(right_frame, font=memo_font)
+        self._task_memo_text.grid(row=2, column=0, sticky=(tk.N, tk.S, tk.E, tk.W),
+                                  padx=(margin_half, margin), pady=(margin_half, margin))
 
     def _add_task_list_button_clicked(self):
         dialog = TaskListDialog(self._root)
@@ -127,7 +133,7 @@ class MainWindow:
 
     def _edit_task_list_button_clicked(self):
         if self._last_selected_task_list is None:
-            ttk_messagebox.showerror('Error', 'Task list is not selected.')
+            ttk_messagebox.showerror('Error', 'No task list is selected.')
             return
 
         dialog = TaskListDialog(self._root, self._last_selected_task_list)
@@ -138,13 +144,64 @@ class MainWindow:
 
     def _remove_task_list_button_clicked(self):
         if self._last_selected_task_list is None:
-            ttk_messagebox.showerror('Error', 'Task list is not selected.')
+            ttk_messagebox.showerror('Error', 'No task list is selected.')
             return
 
         message = 'Really remove {}?'.format(self._last_selected_task_list.name)
         if ttk_messagebox.askokcancel('Confirm', message):
             self._db.delete_task_list(self._last_selected_task_list.id)
             self._update_task_list_listbox()
+
+    def _add_task_button_clicked(self):
+        if self._last_selected_task_list is None:
+            ttk_messagebox.showerror('Error', 'No task list is selected.')
+            return
+
+        id_ = str(uuid.uuid4())
+        parent_id = self._last_selected_task_list.id
+        timestamp = int(datetime.now().timestamp())
+        sort_key = 0.0
+        if self._shown_tasks:
+            sort_key_max = max([task.sort_key for task in self._shown_tasks])
+            sort_key = sort_key_max + 1
+        self._last_selected_task = Task(id_, parent_id, '', '', False, timestamp, timestamp, sort_key)
+        self._db.upsert_task(self._last_selected_task)
+        self._update_task_listbox()
+        self._task_name_entry.focus_set()
+
+    def _remove_task_button_clicked(self):
+        if self._last_selected_task is None:
+            ttk_messagebox.showerror('Error', 'No task is selected.')
+            return
+
+        message = 'Really remove {}?'.format(self._last_selected_task.name)
+        if ttk_messagebox.askokcancel('Confirm', message):
+            self._db.delete_task(self._last_selected_task.id)
+            self._update_task_listbox()
+
+    def _key_pressed(self, event) -> None:
+        if event.widget == self._task_name_entry:
+            self._task_name_entry_key_pressed(event)
+
+    def _task_name_entry_key_pressed(self, event):
+        if event.keysym == 'Return':
+            self._task_name_entry_entered()
+        elif event.keysym == 'Escape':
+            self._task_name_entry.delete(0, tk.END)
+            self._task_name_entry.insert(0, self._last_selected_task.name)
+
+    # noinspection PyUnusedLocal
+    def _task_name_entry_focused_out(self, event):
+        self._task_name_entry_entered()
+
+    def _task_name_entry_entered(self):
+        if self._last_selected_task is None:
+            ttk_messagebox.showerror('Error', 'No task is selected.')
+            return
+
+        self._last_selected_task.name = self._task_name_entry.get()
+        self._db.upsert_task(self._last_selected_task)
+        self._update_task_listbox()
 
     # noinspection PyUnusedLocal
     def _task_list_listbox_selected(self, event):
@@ -160,55 +217,62 @@ class MainWindow:
         index = 0
         index_to_select = 0
         for task_list in self._shown_task_lists:
-            self._task_list_listbox.insert(index, task_list.name)
-            if self._last_selected_task_list and self._last_selected_task_list.id == task_list.id:
-                index_to_select = index
+            label = task_list.name if task_list.name else 'Empty'
+            self._task_list_listbox.insert(index, label)
+            if self._last_selected_task_list and self._last_selected_task_list.sort_key > task_list.sort_key:
+                index_to_select += 1
             index += 1
         if self._shown_task_lists:
+            index_to_select = min(index_to_select, len(self._shown_task_lists) - 1)
             self._task_list_listbox.selection_set(index_to_select)
         self._update_task_listbox()
 
     def _update_task_listbox(self):
+        self._last_selected_task_list = self._get_selected_task_list()
+        self._task_listbox.delete(0, tk.END)
+        if self._last_selected_task_list:
+            self._shown_tasks = self._db.get_tasks(parent_id=self._last_selected_task_list.id)
+            index = 0
+            index_to_select = 0
+            for task in self._shown_tasks:
+                label = task.name if task.name else 'Empty'
+                self._task_listbox.insert(index, label)
+                if self._last_selected_task and self._last_selected_task.sort_key > task.sort_key:
+                    index_to_select += 1
+                index += 1
+            if self._shown_tasks:
+                index_to_select = min(index_to_select, len(self._shown_tasks) - 1)
+                self._task_listbox.selection_set(index_to_select)
+        else:
+            self._shown_tasks = []
+        self._update_task_controls()
+
+    def _get_selected_task_list(self) -> Optional[TaskList]:
         selected_task_list: Optional[TaskList] = None
         selected_indices = self._task_list_listbox.curselection()
         if selected_indices:
             selected_task_list = self._shown_task_lists[selected_indices[0]]
-        if selected_task_list == self._last_selected_task_list:
-            return
+        return selected_task_list
 
-        self._shown_tasks = self._db.get_tasks(parent_id=selected_task_list.id)
-        self._last_selected_task_list = selected_task_list
-        self._task_listbox.delete(0, tk.END)
-        index = 0
-        index_to_select = 0
-        for task in self._shown_tasks:
-            self._task_listbox.insert(index, task.name)
-            if self._last_selected_task and self._last_selected_task.id == task.id:
-                index_to_select = index
-            index += 1
-        if self._shown_tasks:
-            self._task_listbox.selection_set(index_to_select)
-        self._update_task_controls()
+    def _update_task_controls(self) -> None:
+        self._last_selected_task = self._get_selected_task()
+        self._task_name_entry.delete(0, tk.END)
+        self._task_memo_text.delete(1.0, tk.END)
+        if self._last_selected_task:
+            self._task_name_entry.config(state=tk.NORMAL)
+            self._task_name_entry.insert(tk.END, self._last_selected_task.name)
+            self._task_memo_text.config(state=tk.NORMAL)
+            self._task_memo_text.insert(tk.END, self._last_selected_task.name)
+        else:
+            self._task_name_entry.config(state=tk.DISABLED)
+            self._task_memo_text.config(state=tk.DISABLED)
 
-    def _update_task_controls(self):
+    def _get_selected_task(self) -> Optional[Task]:
         selected_task: Optional[Task] = None
         selected_indices = self._task_listbox.curselection()
         if selected_indices:
             selected_task = self._shown_tasks[selected_indices[0]]
-        if selected_task == self._last_selected_task:
-            return
-
-        self._last_selected_task = selected_task
-        self._name_entry.delete(0, tk.END)
-        self._memo_text.delete(1.0, tk.END)
-        if self._last_selected_task:
-            self._name_entry.config(state=tk.NORMAL)
-            self._name_entry.insert(tk.END, selected_task.name)
-            self._memo_text.config(state=tk.NORMAL)
-            self._memo_text.insert(tk.END, selected_task.name)
-        else:
-            self._name_entry.config(state=tk.DISABLED)
-            self._memo_text.config(state=tk.DISABLED)
+        return selected_task
 
     def show(self):
         self._root.mainloop()
