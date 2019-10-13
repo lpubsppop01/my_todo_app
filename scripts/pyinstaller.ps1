8d1af5e15b0ab1fef40ec321729477c7fccece8f
lpubsppop01 <lpubsppop01@gmail.com>
2019-10-13 10:28:32 +0900

    Fix pyinstaller.ps1

--- scripts/pyinstaller.ps1
+++ scripts/pyinstaller.ps1
@@ -1,6 +1,11 @@
 #! /bin/env pwsh
 
+$pyinstaller = pyinstaller
+if ($env:PYTHON -ne '') {
+    $pyinstaller = Join-Path $env:PYTHON 'Scripts\pyinstaller'
+}
+
 $projectPath = Split-Path -Parent $PSScriptRoot
 cd $projectPath
-pyinstaller --noconsole .\my_todo_app\app\main.py --icon .\images\myicon_my_todo.ico -y
+&$pyinstaller --noconsole .\my_todo_app\app\main.py --icon .\images\myicon_my_todo.ico -y
 robocopy /mir images dist\main\images /xf *.svg
